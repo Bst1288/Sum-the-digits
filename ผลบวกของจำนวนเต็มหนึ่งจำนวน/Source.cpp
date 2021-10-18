a9b82c84fda4e32e68a63e5ba08b8d0b11241d5f
Bst1288 <88548302+Bst1288@users.noreply.github.com>
2021-10-18 21:06:02 +0700

    Add files via upload

---
+++ ผลบวกของจำนวนเต็มหนึ่งจำนวน/Source.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+
+int main() {
+	int num;
+	
+	printf("Enter number : ");
+	scanf_s("%d", &num);
+	printf("%d", num);
+
+	while (num > 9) 
+	{
+		printf(" -> ");
+
+		int sum = 0;
+		while (num > 0)
+		{
+			sum = sum + (num % 10);
+			num = num / 10;
+		}
+		num = sum;
+		printf("%d", num);
+	}
+
+	return 0;
+}
